a1052e1ee3c8a540c41eaa47dff8f49fb1b126db
BugHunter-X <askzenai@gmail.com>
2025-01-04 08:48:05 +0100

    Add quotaExceededBug.js file

---
+++ quotaExceededBug.js
@@ -0,0 +1,1 @@
+The Firebase Realtime Database sometimes throws a `QuotaExceededError` even when the app doesn't seem to be exceeding any quotas.  This can manifest as seemingly random failures to write data.  The error message itself isn't always descriptive enough to pinpoint the root cause.
